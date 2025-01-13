fd2adbdb91e8fdcd562310badeb95dc080564b4f
almog19902 <almog19902@gmail.com>
2025-01-13 21:48:03 +0200

    client side

--- Chat/Chat/ChatClient/ClientDetails.cs
+++ Chat/Chat/ChatClient/ClientDetails.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.socket;
+﻿using System.Net;
+using System.Net.Sockets;
 
 namespace ChatClient
 {
-    public class ClientDetails
-    {
-        public IPAddress _ipAddress;
-        public int _port;
-        public string _username;
-        public string _reqUserToChatWith;
-        public socket 
+	public class ClientDetails
+	{
+		public IPAddress _ipAddress;
+		public int _port;
+		public string _username;
+		public string _reqUserToChatWith;
+		public Socket _socket;
 
-    }
+		//public ClientDetails(IPAddress ipAddress, int port, string username,
+		//							string reqUserToChatWith, Socket socket)
+		//{
+		//	_ipAddress = ipAddress;
+		//	_port = port;
+		//	_username = username;
+		//	_reqUserToChatWith = reqUserToChatWith;
+		//	_socket = socket;
+		//}
+	}
 }

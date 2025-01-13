d91bb09591604d535721cfc78b84f38a4d1e097e
almog19902 <almog19902@gmail.com>
2025-01-13 20:00:46 +0200

    chat130125

---
+++ Chat/Chat/ChatClient/ClientDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.socket;
+
+namespace ChatClient
+{
+    public class ClientDetails
+    {
+        public IPAddress _ipAddress;
+        public int _port;
+        public string _username;
+        public string _reqUserToChatWith;
+        public socket 
+
+    }
+}
